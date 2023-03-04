abfa79b32c511036d94cd58398a111ae6a65aac8
Alistair77 <106591699+Alistair77@users.noreply.github.com>
2023-03-04 23:16:02 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,84 @@
+
+var numberOfdrum = document.querySelectorAll(".drum").length;
+                            //                ^this point shows its a class.
+
+
+
+  for (var i = 0;i < numberOfdrum; i++) {
+
+
+    document.querySelectorAll(".drum")[i].addEventListener("click" , function () {
+
+       //var audio = new Audio("sounds/tom-1.mp3");
+        //audio.play();
+
+        var buttonSelected = this.innerHTML;
+         makeSound(buttonSelected);
+         buttonAnimation(buttonSelected);
+
+
+
+      });
+
+
+  }
+
+
+
+  document.addEventListener("keypress" , function (event) {
+
+     //var audio = new Audio("sounds/tom-1.mp3");
+      //audio.play();
+var buttonSelecte = event.key;
+  makeSound(buttonSelecte);
+  buttonAnimation(buttonSelecte);
+    });
+
+function makeSound(event){
+
+  switch (event) {
+    case "w":
+    var tom1 = new Audio("sounds/tom-1.mp3");
+     tom1.play();
+      break;
+
+      case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+       tom2.play();
+        break;
+
+        case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+         tom3.play();
+          break;
+          case "d":
+          var tom4 = new Audio("sounds/tom-4.mp3");
+           tom4.play();
+            break;
+            case "j":
+            var snare = new Audio("sounds/snare.mp3");
+             snare.play();
+              break;
+              case "k":
+              var crash = new Audio("sounds/crash.mp3");
+               crash.play();
+                break;
+                case "l":
+                var kick = new Audio("sounds/kick-bass.mp3");
+                 kick.play();
+                  break;
+
+
+    default:
+console.log(buttonSelecte);
+  }
+}
+
+  function buttonAnimation(currentKey){
+
+    var presse = document.querySelector("." + currentKey);
+
+    presse.classList.add("pressed");
+
+
+   }
